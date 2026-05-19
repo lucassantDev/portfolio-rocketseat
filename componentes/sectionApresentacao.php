dfe04491139527837941da68a1be30abc2c04e31
lucassant <lucasfdesantana095@gmail.com>
2026-05-19 19:42:11 -0300

    feat: pasta de componentes

---
+++ componentes/sectionApresentacao.php
@@ -0,0 +1,15 @@
+<?php
+
+    $stacks = ['Github', 'PHP', 'CSS', 'HTML', 'JavaScript'];
+
+?>
+
+
+<div class="text-center">
+    <p class="text-gray-100 text-lg">Hello World! Meu nome é <span class="text-red-400 ">Lucas Ferreira</span> e sou</p>
+    <h1 class="font-bold text-5xl my-2 text-gray-100">Desenvolvedor PHP</h1>
+    <p class="text-gray-400 text-lg">
+        Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão <br>
+        pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.
+    </p>
+</div>
