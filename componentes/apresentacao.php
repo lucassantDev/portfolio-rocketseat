5ef36ca0241cae656daaaa6d7e53968d3a023193
lucassant <lucasfdesantana095@gmail.com>
2026-05-20 11:20:10 -0300

    arquivo de apresentacao.php e projetos.php

---
+++ componentes/apresentacao.php
@@ -0,0 +1,19 @@
+<section class="bg-[url('./img/Background_Contacts.png')] bg-cover bg-center h-screen flex flex-col items-center justify-center">
+    <img src="" alt="imagem de Lucas" class="my-24 rounded-xl border-red-50 border-4">
+    <div class="text-center">
+        <p class="text-gray-100 text-lg my-2">Hello World! Meu nome é <span class="text-red-400 ">Lucas Ferreira</span> e sou</p>
+        <h1 class="font-bold text-5xl my-2 text-gray-100">Desenvolvedor PHP</h1>
+        <p class="text-gray-400 text-lg my-2">
+            Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão <br>
+            pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.
+        </p>
+    </div>
+
+    <div class="w-84 h-24 flex items-center justify-between">
+        <p class="bg-green-500 p-2 rounded-xl font-semibold">Github</p>
+        <p class="bg-purple-500 p-2 rounded-xl font-semibold">PHP</p>
+        <p class="bg-blue-500 p-2 rounded-xl font-semibold">CSS</p>
+        <p class="bg-red-500 p-2 rounded-xl font-semibold">HTML</p>
+        <p class="bg-yellow-500 p-2 rounded-xl font-semibold">Javascript</p>
+    </div>
+</section>
---
+++ componentes/projetos.php
@@ -0,0 +1,6 @@
+<section class="bg-[url(./img/Background_Intro.png)] bg-cover bg-center bg-mist-900 h-screen flex items-center justify-center">
+    <div class="w-full text-center">
+        <p class="text-red-400 font-semibold text-2xl">Meu trabalho</p>
+        <h2 class="font-bold text-3xl text-gray-100">Veja os projetos em destaque!</h2>
+    </div>
+</section>
