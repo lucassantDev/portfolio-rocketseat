549953f53b9a83b4fa8a2e88b78a65fc78816c48
lucassant <lucasfdesantana095@gmail.com>
2026-05-20 11:22:14 -0300

    feat: estrutura modularizada do projeto

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"
+        />
+        <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"
+        />
+        <meta charset="UTF-8"/>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <title>Portfólio</title>
+    </head>
+    <body>
+        <main class="w-auto">
+
+            
+            
+            <?php include('./componentes/projetos.php');?>
+
+            <?php include('./componentes/contatos.php');?>
+
+        </main>
+    </body>
+</html>
