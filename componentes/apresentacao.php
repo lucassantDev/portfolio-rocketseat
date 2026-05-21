b640e03521ac45a75b0ea94057f963a24ad6e73c
lucassant <lucasfdesantana095@gmail.com>
2026-05-21 16:55:20 -0300

    feat: adicionando imagem

--- componentes/apresentacao.php
+++ componentes/apresentacao.php
@@ -1,5 +1,6 @@
+
 <section class="bg-[url('./img/Background_Contacts.png')] bg-cover bg-center h-screen flex flex-col items-center justify-center">
-    <img src="" alt="imagem de Lucas" class="my-24 rounded-xl border-red-50 border-4">
+    <img src="./img/lucassant-02.jpg" alt="imagem de Lucas" class="my-24 rounded-xl border-red-50 border-1 ">
     <div class="text-center">
         <p class="text-gray-100 text-lg my-2">Hello World! Meu nome é <span class="text-red-400 ">Lucas Ferreira</span> e sou</p>
         <h1 class="font-bold text-5xl my-2 text-gray-100">Desenvolvedor PHP</h1>
