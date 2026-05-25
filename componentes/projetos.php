6b6cf1bef7bb8455384f6812936509812330a0ba
lucassant <lucasfdesantana095@gmail.com>
2026-05-24 23:23:15 -0300

    modularizando estrutura em php

--- componentes/projetos.php
+++ componentes/projetos.php
@@ -1,6 +1,23 @@
-<section class="bg-[url(./img/Background_Intro.png)] bg-cover bg-center bg-mist-900 h-screen flex items-center justify-center">
-    <div class="w-full text-center">
-        <p class="text-red-400 font-semibold text-2xl">Meu trabalho</p>
-        <h2 class="font-bold text-3xl text-gray-100">Veja os projetos em destaque!</h2>
+<?
+
+    $info_card = []
+
+?>
+<div class="w-full h-1/2 border-2 grid grid-cols-2 gap-4 items-center justify-items-center">
+    <div class="w-2/3 h-54 bg-gray-800 flex justify-center rounded-xl">
+        <img src="./img/image-1.png" class="scale-85 rounded-xl" alt="">
+
+        <div class="border-2 w-full flex flex-col items-left justify-center px-4">
+            <div>
+                <h2 class="font-bold text-gray-200 text-lg">Página de receita</h2>
+                <p class="text-gray-400">Página com o passo a passo de uma receita para cupcakes</p>
+            </div>
+            <div class="w-full h-24 flex items-center justify-start gap-2">
+                <p class="bg-purple-500 p-2 rounded-xl font-semibold">PHP</p>
+                <p class="bg-blue-500 p-2 rounded-xl font-semibold">CSS</p>
+                <p class="bg-red-500 p-2 rounded-xl font-semibold">HTML</p>
+                <p class="bg-yellow-500 p-2 rounded-xl font-semibold">Javascript</p>
+            </div>
+        </div>
     </div>
-</section>
+</div>
